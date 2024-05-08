30255c07e82d0e8db28754446e29518b888dc603
Ajmeri Guniya <joyasuvi@gmail.com>
2024-05-08 23:50:33 +0530

    container config

---
+++ 02_basics/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from "./chai";
+function App() {
+  return (
+    <>
+      <Chai />
+      <h3>hii</h3>;
+    </>
+  );
+}
+
+export default App;
---
+++ 02_basics/src/chai.jsx
@@ -0,0 +1,4 @@
+function Chai() {
+  return <h3> Hello Ajmeri</h3>;
+}
+export default Chai;

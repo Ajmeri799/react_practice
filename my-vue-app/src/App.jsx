7252cf172aeddf1f50f81e207b8b98695ced44a5
Ajmeri Guniya <joyasuvi@gmail.com>
2024-05-08 21:41:51 +0530

    initial commit

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+function App() {
+  return (
+    <>
+      <h1>chai our code | Ajmeri Gudiya </h1>
+      <p>
+        This is my first React code, and I know how to create a new project
+        using Vite library.
+      </p>
+      <h2>Syntax:</h2>
+      <p>
+        -*- npm create vite@latest my-app -*- Select a framework: » React -*-
+        now run -*- npm install //added packages -*- npm run dev
+      </p>
+    </>
+  );
+}
+
+export default App;

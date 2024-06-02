7c08e6d6a671e28b3fd9d72106dedf15f5cd0a54
Ajmeri Guniya <joyasuvi@gmail.com>
2024-06-02 22:29:34 +0530

    contaimer config

---
+++ 08_miniContaxt/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import UserContextProvider from "./Context/UserContextProvider";
+import Profile from "./component/Profile";
+import Login from "./component/Login";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1> Click on the Vite and React logos to learn more</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;

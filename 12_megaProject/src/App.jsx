6990440c3b0e59962da2216292357ace69d248e6
Ajmeri Guniya <joyasuvi@gmail.com>
2024-06-14 19:29:55 +0530

    config mega project

---
+++ 12_megaProject/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1 className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </h1>
+    </>
+  );
+}
+
+export default App;
